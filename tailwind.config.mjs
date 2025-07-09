42e7b45d1020c85cf7a579dc034b26c5a335b354
David Pérez <taborlindp@gmail.com>
2025-07-09 19:15:12 +0200

    tweak tailwind config and add detras-de-frappe page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,35 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
       colors: {
-        default: 'var(--color-background)',
-        offset: 'var(--color-background-offset)',
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        card: "var(--color-card)",
       },
       textColor: {
-        default: 'var(--color-text)',
-        offset: 'var(--color-text-offset)',
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
       },
       backgroundColor: {
-        default: 'var(--color-background)',
-        offset: 'var(--color-background-offset)',
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+        card: "var(--color-card)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type"), require("@tailwindcss/typography")],
 } 
